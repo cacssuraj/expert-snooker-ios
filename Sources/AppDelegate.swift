97a39fbf3d23e2ec0f778bf95179db7098062200
cacssuraj <69434797+cacssuraj@users.noreply.github.com>
2026-09-21 13:34:14 +0530

    Expert Snooker iOS: WKWebView wrapper + unsigned IPA workflow

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,45 @@
+import UIKit
+import WebKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let win = UIWindow(frame: UIScreen.main.bounds)
+        win.rootViewController = GameViewController()
+        win.makeKeyAndVisible()
+        window = win
+        UIApplication.shared.isIdleTimerDisabled = true
+        return true
+    }
+}
+
+class GameViewController: UIViewController {
+    private var webView: WKWebView!
+
+    override var prefersStatusBarHidden: Bool { true }
+    override var prefersHomeIndicatorAutoHidden: Bool { true }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .landscape }
+    override var shouldAutorotate: Bool { true }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.05, green: 0.05, blue: 0.12, alpha: 1)
+        let config = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+        webView = WKWebView(frame: view.bounds, configuration: config)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView.scrollView.isScrollEnabled = false
+        webView.scrollView.bounces = false
+        webView.scrollView.contentInsetAdjustmentBehavior = .never
+        webView.isOpaque = false
+        webView.backgroundColor = view.backgroundColor
+        view.addSubview(webView)
+        if let url = Bundle.main.url(forResource: "game", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
+    }
+}
